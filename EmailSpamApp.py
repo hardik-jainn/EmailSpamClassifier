95d25a3bc3ede19aabade55191939948a1f830df
Hardik Jain <110124726+hardik-jainn@users.noreply.github.com>
2023-09-11 04:06:00 +0530

    Update EmailSpamApp.py

--- EmailSpamApp.py
+++ EmailSpamApp.py
@@ -40,13 +40,13 @@ st.title("Email Spam Classifier")
 input_sms = st.text_area('Enter the message')
 
 if st.button('Predict'):
-    # 1. PREPROCESS'''
+    # 1. PREPROCESS
     transformed_sms = transform_text(input_sms)
-    # 2. VECTORIZE'''
+    # 2. VECTORIZE
     vector_input = tfidf.transform([transformed_sms])
-    # 3. PREDICTION'''
+    # 3. PREDICTION
     result = model.predict(vector_input)[0]
-    # 4. DISPLAY'''
+    # 4. DISPLAY
     if result == 1:
         st.header("SPAM")
     else:
